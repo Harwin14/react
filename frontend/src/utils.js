61d3581e535e0f06812ebd67750db4cf38180fd1
Harwin14 <agungharwinn@gmail.com>
2022-12-16 17:23:12 +0700

    searching not done

---
+++ frontend/src/utils.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const makeRequestCreator = () => {
+  let token;
+  let { query } = (`http://localhost:3000/users`);
+
+  return (query) => {
+    // Check if we made a request
+    if(token){
+      // Cancel the previous request before making a new request
+      token.cancel()
+    }
+    // Create a new CancelToken
+    token = axios.CancelToken.source()
+    try{
+      const res = await axios(query, {cancelToken: cancel.token})
+      const result = data.data
+      return result;
+    } catch(error) {
+        if(axios.isCancel(error)) {
+          // Handle if request was cancelled
+          console.log('Request canceled', error.message);
+        } else {
+          // Handle usual errors
+          console.log('Something went wrong: ', error.message)
+        }
+    }
+  }
+}
+
+export const search = makeRequestCreator()
